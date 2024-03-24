b73321411fbf409d64664e63780982eac5953911
Marino McGalla <marino@Marinos-MacBook-Pro.local>
2024-03-24 17:22:32 -0400

    missed firebase

--- services/firebaseStuff.py
+++ services/firebaseStuff.py
@@ -1,14 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from dev.devTokens import google_creds
+from dev.devTokens import firestore_creds
 
-firebase_cred = credentials.Certificate(google_creds)
+firebase_cred = credentials.Certificate(firestore_creds)
 firebase_admin.initialize_app(firebase_cred)
 
 db = firestore.client()
 
-# # Create a reference to the 'hello-world' collection
-# collection_ref = db.collection('hello-world')
+# Create a reference to the 'hello-world' collection
+collection_ref = db.collection('hello-world')
 
-# # Add a document with ID 'greeting' and data {'message': 'Hello, world!'}
-# collection_ref.document('greeting').set({'message': 'Hello, world!'})
+# Add a document with ID 'greeting' and data {'message': 'Hello, world!'}
+collection_ref.document('greeting').set({'message': 'Hello, world!'})
