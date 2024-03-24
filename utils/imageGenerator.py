2a16460e2cd6705f6073f42ba667160024b7087e
Untuxed <lcm7498@gmail.com>
2024-03-24 16:38:00 -0400

    Refactored imageGenerator.py to support .svg loading

--- utils/imageGenerator.py
+++ utils/imageGenerator.py
@@ -3,19 +3,8 @@ from datetime import datetime
 import services.cellOperations as cellOperations
 import services.sheets as sheets
 import random
+import cairosvg
 
-
-# def edit_text_in_file(o_fp, n_fp, old_text, new_text):
-#         # Read the contents of the file
-#         with open(o_fp, 'r') as file:
-#             file_content = file.read()
-#         # Perform the text replacement
-#         modified_content = file_content.replace(old_text, new_text)
-#         with open(o_fp, 'w') as file:
-#             file.write(modified_content)
-#         cairosvg.svg2png(url=o_fp, write_to=n_fp)
-
-# Forwards Pixel Range 860 x 200 to 1950 x 715
 
 def imageGenerator():
     def numberLookup(playerName):
@@ -32,13 +21,57 @@ def imageGenerator():
     def randomTextColor():
         return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
-    Base_Lineup_Image = cv2.imread('./BaseLineupCard.png')
-    Dennis_Base_Lineup_Image = cv2.imread('./Dennis_BaseLineupCard.png')
+    def addGameInfo(baseSVGFilePath, newImageFilePath, opponentName, gameTime):
+        temp_SVG_FilePath = './../resources/images/temp.svg'
 
-    Font_Size = 1.5
-    Font_Weight = 3
-    Font_Color = (0, 0, 0)
-    Font_Type = cv2.FONT_HERSHEY_DUPLEX
+        with open(baseSVGFilePath, 'r') as file:
+            base_SVG_Data = file.read()
+
+        SVG_Game_Info = base_SVG_Data.replace('### REPLACE ME ###', opponentName + gameTime)
+
+        with open(temp_SVG_FilePath, 'w') as file:
+            file.write(SVG_Game_Info)
+
+        cairosvg.svg2png(url=temp_SVG_FilePath, write_to=newImageFilePath)
+
+    def addPlayerLineup(baseImage, playerText, x_anchor, y_location, Font_Color=(0, 0, 0)):
+        Font_Size = 1.5
+        Font_Weight = 3
+        Font_Type = cv2.FONT_HERSHEY_DUPLEX
+
+        text_Width = cv2.getTextSize(playerText, Font_Type, fontScale=Font_Size, thickness=Font_Weight)
+        return cv2.putText(
+            baseImage,
+            playerText,
+            (int(x_anchor - (text_Width[0][0] / 2)), y_location),
+            fontFace=Font_Type,
+            fontScale=Font_Size,
+            color=Font_Color,
+            thickness=Font_Weight
+        )
+
+    Game_Dates = cellOperations.Get_Cell_Range(sheets.RSVP_SHEET_RANGE)[0]
+    Earliest_Game_Date = Game_Dates[0]
+
+    if Earliest_Game_Date:
+        addGameInfo('./../resources/images/BaseLineupCard.svg',
+                    './../resources/images/BaseLineupCard.png',
+                    'OpponentName',
+                    datetime.strptime(Earliest_Game_Date, '%H:%M')
+                    )
+
+        addGameInfo('./../resources/images/Dennis_BaseLineupCard.svg',
+                    './../resources/images/Dennis_BaseLineupCard.png',
+                    'OpponentName',
+                    datetime.strptime(Earliest_Game_Date, '%H:%M')
+                    )
+    else:
+        return
+
+    Earliest_Game_Date = datetime.strptime(Earliest_Game_Date, '%Y-%m-%d %H:%M:%S')
+
+    Base_Lineup_Image = cv2.imread('./../resources/images/BaseLineupCard.png')
+    Dennis_Base_Lineup_Image = cv2.imread('./../resources/images/Dennis_BaseLineupCard.png')
 
     existing_players = cellOperations.get_players()
 
@@ -85,166 +118,45 @@ def imageGenerator():
     Forward_Y_Spacing = int(515 / len(Forwards))
     Defense_Y_Spacing = int(500 / len(Defense))
 
-    for i, Line in enumerate(Forwards):
+    for Line_Number, Line in enumerate(Forwards):
         LW_Text = numberLookup(Line[0])
         C_Text = numberLookup(Line[1])
         RW_Text = numberLookup(Line[2])
 
-        LW_Width = cv2.getTextSize(LW_Text, Font_Type, fontScale=Font_Size, thickness=Font_Weight)
-        C_Width = cv2.getTextSize(C_Text, Font_Type, fontScale=Font_Size, thickness=Font_Weight)
-        RW_Width = cv2.getTextSize(RW_Text, Font_Type, fontScale=Font_Size, thickness=Font_Weight)
+        _ = addPlayerLineup(Base_Lineup_Image, LW_Text, 947.5, 200 + Line_Number * Forward_Y_Spacing)
+        _ = addPlayerLineup(Base_Lineup_Image, C_Text, 1327.5, 200 + Line_Number * Forward_Y_Spacing)
+        _ = addPlayerLineup(Base_Lineup_Image, RW_Text, 1702.5, 200 + Line_Number * Forward_Y_Spacing)
 
-        cv2.putText(
-            Base_Lineup_Image,
-            LW_Text,
-            (int(760 + (375 - LW_Width[0][0]) / 2), 200 + i * Forward_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=Font_Color,
-            thickness=Font_Weight
-        )
+        _ = addPlayerLineup(Dennis_Base_Lineup_Image, LW_Text, 947.5, 200 + Line_Number * Forward_Y_Spacing,
+                            randomTextColor())
+        _ = addPlayerLineup(Dennis_Base_Lineup_Image, C_Text, 1327.5, 200 + Line_Number * Forward_Y_Spacing,
+                            randomTextColor())
+        _ = addPlayerLineup(Dennis_Base_Lineup_Image, RW_Text, 1702.5, 200 + Line_Number * Forward_Y_Spacing,
+                            randomTextColor())
 
-        cv2.putText(
-            Base_Lineup_Image,
-            C_Text,
-            (int(1327.5 - C_Width[0][0] / 2), 200 + i * Forward_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=Font_Color,
-            thickness=Font_Weight
-        )
-
-        cv2.putText(
-            Base_Lineup_Image,
-            RW_Text,
-            (int(1515 + (375 - RW_Width[0][0]) / 2), 200 + i * Forward_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=Font_Color,
-            thickness=Font_Weight
-        )
-
-        randomColor = randomTextColor()
-
-        cv2.putText(
-            Dennis_Base_Lineup_Image,
-            LW_Text,
-            (int(760 + (375 - LW_Width[0][0]) / 2), 200 + i * Forward_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=randomColor,
-            thickness=Font_Weight
-        )
-
-        randomColor = randomTextColor()
-
-        cv2.putText(
-            Dennis_Base_Lineup_Image,
-            C_Text,
-            (int(1327.5 - C_Width[0][0] / 2), 200 + i * Forward_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=randomColor,
-            thickness=Font_Weight
-        )
-
-        randomColor = randomTextColor()
-
-        cv2.putText(
-            Dennis_Base_Lineup_Image,
-            RW_Text,
-            (int(1515 + (375 - RW_Width[0][0]) / 2), 200 + i * Forward_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=randomColor,
-            thickness=Font_Weight
-        )
-
-    for i, Line in enumerate(Defense):
+    for Line_Number, Line in enumerate(Defense):
         LD_Text = numberLookup(Line[0])
         RD_Text = numberLookup(Line[1])
 
-        LD_Width = cv2.getTextSize(LD_Text, Font_Type, fontScale=Font_Size, thickness=Font_Weight)
-        RD_Width = cv2.getTextSize(RD_Text, Font_Type, fontScale=Font_Size, thickness=Font_Weight)
+        _ = addPlayerLineup(Base_Lineup_Image, LD_Text, 380, 870 + Line_Number * Defense_Y_Spacing)
+        _ = addPlayerLineup(Base_Lineup_Image, RD_Text, 1611.25, 870 + Line_Number * Defense_Y_Spacing)
 
-        cv2.putText(
-            Base_Lineup_Image,
-            LD_Text,
-            (int(760 + (567.5 - LD_Width[0][0]) / 2), 870 + i * Defense_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=Font_Color,
-            thickness=Font_Weight
-        )
-
-        cv2.putText(
-            Base_Lineup_Image,
-            RD_Text,
-            (int(1327.5 + (567.5 - RD_Width[0][0]) / 2), 870 + i * Defense_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=Font_Color,
-            thickness=Font_Weight
-        )
-
-        randomColor = randomTextColor()
-
-        cv2.putText(
-            Dennis_Base_Lineup_Image,
-            LD_Text,
-            (int(760 + (567.5 - LD_Width[0][0]) / 2), 870 + i * Defense_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=randomColor,
-            thickness=Font_Weight
-        )
-
-        randomColor = randomTextColor()
-
-        cv2.putText(
-            Dennis_Base_Lineup_Image,
-            RD_Text,
-            (int(1327.5 + (567.5 - RD_Width[0][0]) / 2), 870 + i * Defense_Y_Spacing),
-            fontFace=Font_Type,
-            fontScale=Font_Size,
-            color=randomColor,
-            thickness=Font_Weight
-        )
+        _ = addPlayerLineup(Dennis_Base_Lineup_Image, LD_Text, 947.5, 200 + Line_Number * Forward_Y_Spacing,
+                            randomTextColor())
+        _ = addPlayerLineup(Dennis_Base_Lineup_Image, RD_Text, 947.5, 200 + Line_Number * Forward_Y_Spacing,
+                            randomTextColor())
 
     G_Text = numberLookup(Goalie[0][1])
 
-    Lineup_Image_W_Text = cv2.putText(
-        Base_Lineup_Image,
-        G_Text,
-        (int(1327.5 - C_Width[0][0] / 2), 1500),
-        fontFace=Font_Type,
-        fontScale=Font_Size,
-        color=Font_Color,
-        thickness=Font_Weight
-    )
-
-    randomColor = randomTextColor()
-
-    Dennis_Lineup_Image_W_Text = cv2.putText(
-        Dennis_Base_Lineup_Image,
-        G_Text,
-        (int(1327.5 - C_Width[0][0] / 2), 1500),
-        fontFace=Font_Type,
-        fontScale=Font_Size,
-        color=randomColor,
-        thickness=Font_Weight
-    )
-
-    Game_Dates = cellOperations.Get_Cell_Range(sheets.RSVP_SHEET_RANGE)[0]
-    Earliest_Game_Date = Game_Dates[0]
-
-    Earliest_Game_Date = datetime.strptime(Earliest_Game_Date, '%Y-%m-%d %H:%M:%S')
+    Lineup_Image_W_Text = addPlayerLineup(Base_Lineup_Image, G_Text, 1327.5, 1500)
+    Dennis_Lineup_Image_W_Text = addPlayerLineup(Base_Lineup_Image, G_Text, 1327.5, 1500,
+                                                 randomTextColor())
 
     if Earliest_Game_Date:
         timestamp = (Earliest_Game_Date - datetime(1970, 1, 1)).total_seconds()
-        cv2.imwrite(f'./LineUpWithName_{timestamp}.png', Lineup_Image_W_Text)
-        cv2.imwrite(f'./DennisLineUpWithName_{timestamp}.png', Dennis_Lineup_Image_W_Text)
-        return f'./LineUpWithName_{timestamp}.png'
+        cv2.imwrite(f'./../resources/images/LineUpWithName_{timestamp}.png', Lineup_Image_W_Text)
+        cv2.imwrite(f'./../resources/images/DennisLineUpWithName_{timestamp}.png', Dennis_Lineup_Image_W_Text)
+        return f'./../resources/images/LineUpWithName_{timestamp}.png'
     else:
         return
 
@@ -257,6 +169,7 @@ def pullImage():
 
     if Earliest_Game_Date:
         timestamp = (Earliest_Game_Date - datetime(1970, 1, 1)).total_seconds()
-        return f'./LineUpWithName_{timestamp}.png', f'./DennisLineUpWithName_{timestamp}.png'
+        return (f'./../resources/images/LineUpWithName_{timestamp}.png',
+                f'./../resources/images/DennisLineUpWithName_{timestamp}.png')
     else:
         return
