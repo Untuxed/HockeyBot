eba8023fc76e44f4e542dedb5ef689cd61e337dc
Untuxed <lcm7498@gmail.com>
2024-04-15 16:57:41 -0400

    Changed stats message to be a personal message

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-import os
-import re
-import json
-from tabulate import tabulate #do we still need this?
-from datetime import datetime, timedelta #or this?
-import time #or this?
-from services.googleStuff import *
-from services.discordStuff import *
-from services.sheets import *
-from utils.lineBuilder import *
-from utils.lineBuilderv2 import *
 from commands.firebaseSlashCommands import *
 from dev.devTokens import discord_token_prod, discord_token_dev
 
@@ -17,18 +6,6 @@ from dev.devTokens import discord_token_prod, discord_token_dev
 async def on_ready():
     print(f'{hockeyBot.user.name} has connected to Discord!')
     await tree.sync(guild=GUILD_ID)
-
-
-@hockeyBot.event
-async def on_scheduled_event_create(event):
-    if event:
-        hockeyBot.get_channel()
-
-
-@hockeyBot.event
-async def on_scheduled_event_user_add(event, user):
-    if user:
-        print(user)
 
 
 hockeyBot.run(discord_token_dev)
---
+++ services/eventInitialization.py
@@ -0,0 +1,13 @@
+from datetime import date
+import discord
+from utils.genericFunctions import get_season_and_game_id
+
+# region initialization function
+def event_initialization():
+    today = date.today()
+
+    schedule_Channel_ID = discord.utils.get(Gu)
+
+    s_ID, event_date = get_season_and_game_id()
+
+# endregion
